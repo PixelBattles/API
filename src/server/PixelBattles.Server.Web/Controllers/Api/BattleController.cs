baec06b99b53071a4c7e6b82224732d7b8e96d1a
ihnat.klimchuk <ihnat.klimchuk@gmail.com>
2018-02-05 02:24:15 +0300

    Test image population was added

--- src/server/PixelBattles.Server.Web/Controllers/Api/BattleController.cs
+++ src/server/PixelBattles.Server.Web/Controllers/Api/BattleController.cs
@@ -4,7 +4,10 @@ using Microsoft.Extensions.Logging;
 using PixelBattles.Server.BusinessLogic.Managers;
 using PixelBattles.Server.BusinessLogic.Models;
 using PixelBattles.Shared.DataTransfer.Api.Battle;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PixelBattles.Server.Web.Controllers.Api
@@ -40,6 +43,34 @@ namespace PixelBattles.Server.Web.Controllers.Api
             catch (Exception exception)
             {
                 return Exception(exception, "Error while getting battles.");
+            }
+        }
+
+        #warning This method is test only and should be removed
+        [HttpGet("battle/imageTest")]
+        public async Task<IActionResult> GetBattleImageTestAsync()
+        {
+            try
+            {
+                using (Image<Rgb24> image = new Image<Rgb24>(400, 400))
+                {
+                    for (int i = 0; i < image.Height; i++)
+                    {
+                        for (int j = 0; j < image.Width; j++)
+                        {
+                            byte value = ((i + j) % 2) == 0 ? (byte)255 : (byte)0;
+                            image[i, j] = new Rgb24(value, value, value);
+                        }
+                    }
+                    Stream imageStream = new MemoryStream();
+                    image.SaveAsPng(imageStream);
+                    imageStream.Seek(0, SeekOrigin.Begin);
+                    return File(imageStream, "image/png");
+                }
+            }
+            catch (Exception exception)
+            {
+                return Exception(exception, "Error while getting battle image.");
             }
         }
     }
