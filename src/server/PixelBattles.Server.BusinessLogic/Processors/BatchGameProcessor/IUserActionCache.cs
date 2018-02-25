9e7f8323d4cf568be6a6031f33b9375a1d8af1cc
ihnat.klimchuk <ihnat.klimchuk@gmail.com>
2018-02-25 15:01:50 +0300

    Implementing cache

---
+++ src/server/PixelBattles.Server.BusinessLogic/Processors/BatchGameProcessor/IUserActionCache.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PixelBattles.Server.BusinessLogic.Processors
+{
+    public interface IUserActionCache
+    {
+        int SizeLimit { get; set; }
+
+        void Push(UserAction item);
+        
+        void Push(ICollection<UserAction> items);
+
+        void Clear();
+
+        ICollection<UserAction> GetRange(int fromChangeIndex, int toChangeIndex);
+    }
+}
--- src/server/PixelBattles.Server.BusinessLogic/Processors/BatchGameProcessor/UserActionCache.cs
+++ src/server/PixelBattles.Server.BusinessLogic/Processors/BatchGameProcessor/UserActionCache.cs
@@ -1,33 +1,108 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace PixelBattles.Server.BusinessLogic.Processors
 {
-    public class UserActionCache
+    public class UserActionCache : IUserActionCache
     {
         private ReaderWriterLockSlim collectionLock;
+        
+        private UserAction[] userActions;
 
-        private int cacheMaxSize;
-
-        private SortedList<int, UserAction> userActions;
-
-        public UserActionCache(int cacheMaxSize)
+        private int sizeLimit;
+        public int SizeLimit
         {
-            this.cacheMaxSize = cacheMaxSize;
-            this.userActions = new SortedList<int, UserAction>(cacheMaxSize);
-            this.collectionLock = new ReaderWriterLockSlim();
+            get
+            {
+                try
+                {
+                    collectionLock.EnterReadLock();
+                    return userActions.Length;
+                }
+                finally
+                {
+                    collectionLock.ExitReadLock();
+                }
+            }
+            set
+            {
+                try
+                {
+                    collectionLock.EnterWriteLock();
+                    ResizeInternal(value);
+                }
+                finally
+                {
+                    collectionLock.EnterWriteLock();
+                }
+            }
         }
 
-        public void Push(IEnumerable<UserAction> newUserActions)
+        private int minChangeIndex;
+        public int MinChangeIndex
+        {
+            get
+            {
+                try
+                {
+                    collectionLock.EnterReadLock();
+                    return minChangeIndex;
+                }
+                finally
+                {
+                    collectionLock.ExitReadLock();
+                }
+            }
+        }
+
+        private int maxChangeIndex;
+        public int MaxChangeIndex
+        {
+            get
+            {
+                try
+                {
+                    collectionLock.EnterReadLock();
+                    return maxChangeIndex;
+                }
+                finally
+                {
+                    collectionLock.ExitReadLock();
+                }
+            }
+        }
+
+        public UserActionCache(int sizeLimit)
+        {
+            this.sizeLimit = sizeLimit;
+            this.userActions = new UserAction[sizeLimit];
+            this.collectionLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        }
+
+        private void ResizeInternal(int size)
+        {
+            if (size >= userActions.Length)
+            {
+                Array.Resize(ref userActions, size);
+            }
+            else
+            {
+                var newArray = new UserAction[size];
+                int lastIndex = maxChangeIndex - minChangeIndex;
+                int indexCopyFrom = lastIndex - size + 1;
+                Array.Copy(userActions, indexCopyFrom > 0 ? indexCopyFrom : 0, newArray, 0, size);
+                minChangeIndex = userActions[0]?.ChangeIndex ?? 0;
+            }
+        }
+                        
+        public void Push(UserAction item)
         {
             try
             {
                 collectionLock.EnterWriteLock();
-
-                foreach (var userAction in newUserActions)
-                {
-                    AddUserActionInternal(userAction);
-                }
+                PushInternal(item);
             }
             finally
             {
@@ -35,18 +110,122 @@ namespace PixelBattles.Server.BusinessLogic.Processors
             }
         }
 
-        private void AddUserActionInternal(UserAction userAction)
+        private void PushInternal(UserAction item)
         {
+            if (maxChangeIndex == 0)
+            {
+                userActions[0] = item;
+                maxChangeIndex = item.ChangeIndex;
+                minChangeIndex = item.ChangeIndex;
+                return;
+            }
+            
+            int newItemIndex = maxChangeIndex - minChangeIndex + 1;
+            if (userActions.Length - newItemIndex == 0)
+            {
+                ShiftItemsInternal(1);
+                minChangeIndex = userActions[0].ChangeIndex;
+            }
+            userActions[newItemIndex] = item;
+            maxChangeIndex = item.ChangeIndex;
         }
 
-        private void PrepareCollectionSize(int insertSize)
+        private void ShiftItemsInternal(int offset)
         {
-
+            Array.Copy(userActions, offset, userActions, 0, userActions.Length - offset);
         }
 
-        public IEnumerable<UserAction> ResolveUserActions(int fromChangeIndex, int toChangeIndex)
+        public void Push(ICollection<UserAction> items)
         {
-            return null;
+            try
+            {
+                collectionLock.EnterWriteLock();
+                PushInternal(items);
+            }
+            finally
+            {
+                collectionLock.ExitWriteLock();
+            }
         }
+
+        private void PushInternal(ICollection<UserAction> items)
+        {
+            if (items.Count >= userActions.Length)
+            {
+                Array.Copy(items.ToArray(), items.Count - userActions.Length, userActions, 0, userActions.Length);
+                maxChangeIndex = userActions[userActions.Length - 1].ChangeIndex;
+                minChangeIndex = userActions[0].ChangeIndex;
+            }
+            else
+            {
+                if (maxChangeIndex == 0)
+                {
+                    Array.Copy(items.ToArray(), 0, userActions, 0, items.Count);
+                    maxChangeIndex = userActions[items.Count - 1].ChangeIndex;
+                    minChangeIndex = userActions[0].ChangeIndex;
+                }
+                else
+                {
+                    int newItemIndex = maxChangeIndex - minChangeIndex + 1;
+                    int requiredOffset = items.Count + newItemIndex - userActions.Length;
+                    if (requiredOffset > 0)
+                    {
+                        ShiftItemsInternal(requiredOffset);
+                        minChangeIndex = userActions[0].ChangeIndex;
+                    }
+                    Array.Copy(items.ToArray(), 0, userActions, newItemIndex - requiredOffset, items.Count);
+                    maxChangeIndex = userActions[newItemIndex - requiredOffset + items.Count].ChangeIndex;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            try
+            {
+                collectionLock.EnterWriteLock();
+                ClearInternal();
+            }
+            finally
+            {
+                collectionLock.ExitWriteLock();
+            }
+            
+        }
+
+        private void ClearInternal()
+        {
+            Array.Clear(userActions, 0, userActions.Length);
+            minChangeIndex = 0;
+            maxChangeIndex = 0;
+        }
+        
+        public ICollection<UserAction> GetRange(int fromChangeIndex, int toChangeIndex)
+        {
+            try
+            {
+                collectionLock.EnterReadLock();
+                return GetRangeInternal(fromChangeIndex, toChangeIndex);
+            }
+            finally
+            {
+                collectionLock.ExitReadLock();
+            }
+        }
+
+        private ICollection<UserAction> GetRangeInternal(int fromChangeIndex, int toChangeIndex)
+        {
+            if (minChangeIndex > fromChangeIndex || maxChangeIndex < toChangeIndex)
+            {
+                return null;
+            }
+            else
+            {
+                int itemsToCopy = toChangeIndex - fromChangeIndex + 1;
+                UserAction[] resultArray = new UserAction[itemsToCopy];
+                Array.Copy(userActions, fromChangeIndex - minChangeIndex, resultArray, 0, itemsToCopy);
+                return resultArray;
+            }
+        } 
     }
 }
