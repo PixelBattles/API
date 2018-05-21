d65a28a9b664fa8f02974bed7857ba230ff5377a
Ihnat Klimchuk <i.klimchuk@adform.com>
2018-05-21 20:47:44 +0300

    Addinf api client

---
+++ src/client/PixelBattles.Server.Client/ApiClient.cs
@@ -0,0 +1,34 @@
+﻿using PixelBattles.Shared.DataTransfer.Api.Game;
+using System;
+using System.Net.Http;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace PixelBattles.Server.Client
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient httpClient;
+
+        public ApiClient(string baseUrl)
+        {
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+        }
+
+        public async Task<GameDTO> GetGameAsync(Guid gameId, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var response = await httpClient.GetAsync("/api/game/" + gameId);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var game = JsonConvert.DeserializeObject<GameDTO>(responseContent);
+                return game;
+            }
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/client/PixelBattles.Server.Client/IApiClient.cs
@@ -0,0 +1,12 @@
+﻿using PixelBattles.Shared.DataTransfer.Api.Game;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PixelBattles.Server.Client
+{
+    public interface IApiClient
+    {
+        Task<GameDTO> GetGameAsync(Guid gameId, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
