fe61e364472287183df62bade7beb857c76be569
Ihnat Klimchuk <i.klimchuk@adform.com>
2018-08-09 10:04:14 +0300

    Remove unused code

--- src/PixelBattles.Server.Web/Client/PixelBattle/Clients/ApiClient.ts
+++ src/PixelBattles.Server.Web/Client/PixelBattle/Clients/ApiClient.ts
@@ -25,26 +25,4 @@ export class ApiClient implements IApiClient {
             headers);
         return JSON.parse(result);
     }
-
-//    public createHubClient(gameId: string, url: string, token: string): Promise<IHubClient> {
-//        return new Promise<IHubClient>((resolve, reject) => {
-//            reject("Failed to resolve hub");
-//            //let hubClient = new HubClient(url)
-//        });
-//    }
-
-//    public getGameInfo(gameId: string): Game {
-//        if (gameId == "a5239ac0-b598-40e3-8f16-dae522be6e3c") {
-//            return {
-//                gameId: "a5239ac0-b598-40e3-8f16-dae522be6e3c",
-//                height: 1000,
-//                width: 1000,
-//                cooldown: 60,
-//                startDateUTC: "2018-03-06T02:32:46.974Z",
-//                endDateUTC: "2019-03-06T02:32:46.974Z"
-//            };
-//        } else {
-//            return null;
-//        }
-//    }
 }
